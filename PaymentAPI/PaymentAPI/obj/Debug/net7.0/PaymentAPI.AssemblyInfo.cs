00e67570c511c6a4aba576257fc39444aca0e3d8
steynie8 <steynie8@gmail.com>
2024-04-23 23:09:17 +0200

    Created Angular PaymentApp

--- PaymentAPI/PaymentAPI/obj/Debug/net7.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net7.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1dce0077a55695618cdab9f3d8d0a1b138c7394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd719350a2d2a03ee58c5316d571068b596c4b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
