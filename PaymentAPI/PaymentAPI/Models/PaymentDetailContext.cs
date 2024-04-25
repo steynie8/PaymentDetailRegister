e3b67660a999ce3502e2a8f6136f881ffbb27c57
steynie8 <steynie8@gmail.com>
2024-04-25 23:23:48 +0200

    Cleanup and comments.

--- PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -2,6 +2,7 @@
 
 namespace PaymentAPI.Models
 {
+    // Represents a session with the database and can be used to query and save instances of your entities
     public class PaymentDetailContext : DbContext
     {
         public PaymentDetailContext(DbContextOptions options) : base(options)
